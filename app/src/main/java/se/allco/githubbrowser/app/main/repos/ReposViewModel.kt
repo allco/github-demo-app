405ae3a5cb7b665857cc2c069afd19b45a21231c
Alexander Skvortsov <alexander.skvortsov@gmail.com>
2021-08-07 16:39:34 +0200

    integrate LoadableContentViewModel to ReposViewModel

--- app/src/main/java/se/allco/githubbrowser/app/main/repos/ReposViewModel.kt
+++ app/src/main/java/se/allco/githubbrowser/app/main/repos/ReposViewModel.kt
@@ -1,17 +1,14 @@
 package se.allco.githubbrowser.app.main.repos
 
 import android.app.Application
-import androidx.annotation.StringRes
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.subjects.BehaviorSubject
 import se.allco.githubbrowser.R
-import se.allco.githubbrowser.common.ui.addSmartLoadingState
-import se.allco.githubbrowser.common.utils.getString
-import se.allco.githubbrowser.common.utils.map
+import se.allco.githubbrowser.common.ui.LoadableContentViewModel
+import se.allco.githubbrowser.common.ui.attachSmartLoading
 import se.allco.githubbrowser.common.utils.toLiveData
 import timber.log.Timber
 import javax.inject.Inject
@@ -19,91 +16,47 @@ import javax.inject.Inject
 class ReposViewModel @Inject constructor(
     application: Application,
     reposRepository: ReposRepository,
+    val contentViewModel: LoadableContentViewModel,
     private val reposItemViewModelFactory: ReposItemViewModel.Factory,
 ) : AndroidViewModel(application) {
 
     private val disposables = CompositeDisposable()
 
-    private sealed class State {
-        object Initializing : State()
-        object Loading : State()
-        class Content(val data: List<ReposItemViewModel>) : State()
-        class Error(@StringRes val messageRes: Int, val allowRetry: Boolean = true) : State()
-    }
-
-    val showLoading = MutableLiveData(false)
-    val showContent = MutableLiveData(false)
-    val errorMessage = MutableLiveData<String>(null)
-    val showError: LiveData<Boolean> = errorMessage.map { it != null }
-
-    private val retrySubject = BehaviorSubject.createDefault(Unit)
-
     val listItems: LiveData<List<ReposItemViewModel>> =
-        retrySubject
-            .switchMap {
-
+        contentViewModel
+            .runRetryable<List<ReposItemViewModel>> {
                 reposRepository
                     .getRepos()
-                    .map { listRepos ->
-                        onDataReceived(listRepos)
-                    }
-                    .addSmartLoadingState { State.Loading }
-                    .doOnSubscribe { renderState(State.Initializing) }
-                    .doOnNext(::renderState)
-                    .ofType(State.Content::class.java)
-                    .map { it.data }
-                    .onErrorResumeNext(::createErrorHandler)
 
+                    .map { list -> list.map { reposItemViewModelFactory.create(it) } }
+                    .renderStates()
             }
+            .onErrorResumeNext(::createErrorHandler)
             .toLiveData()
 
-    private fun onDataReceived(listRepos: List<ReposRepository.Repo>): State {
-        return when {
-            listRepos.isEmpty() -> State.Error(
-                R.string.main_repos_no_repos_found,
-                allowRetry = false,
-            )
-            else -> State.Content(
-                data = listRepos.map { reposItemViewModelFactory.create(it) },
-            )
-        }
-    }
+    private fun Single<List<ReposItemViewModel>>.renderStates() =
+        doOnSubscribe { contentViewModel.renderInitialisation() }
+            .attachSmartLoading { onShowLoading = { contentViewModel.renderStateLoading() } }
+            .doOnSuccess { list ->
+                when {
+                    list.isEmpty() -> renderStateEmptyList()
+                    else -> contentViewModel.renderStateContentReady()
+                }
+            }
 
     private fun createErrorHandler(err: Throwable): Observable<List<ReposItemViewModel>> {
         Timber.w(err, "ReposViewModel failed")
         return Observable
             .never<List<ReposItemViewModel>>()
-            .doOnSubscribe { renderState(State.Error(R.string.main_repos_error_fetching_data)) }
+            .doOnSubscribe { renderStateErrorFetchData() }
     }
 
-    private fun renderState(state: State) {
-        Timber.v("renderState() $state")
-        when (state) {
-            State.Initializing -> {
-                showContent.postValue(false)
-                showLoading.postValue(false)
-                errorMessage.postValue(null)
-            }
-            State.Loading -> {
-                showContent.postValue(false)
-                errorMessage.postValue(null)
-                showLoading.postValue(true)
-            }
-            is State.Content -> {
-                showLoading.postValue(false)
-                errorMessage.postValue(null)
-                showContent.postValue(true)
-            }
-            is State.Error -> {
-                showContent.postValue(false)
-                showLoading.postValue(false)
-                errorMessage.postValue(getString(state.messageRes))
-            }
-        }
+    private fun renderStateEmptyList() {
+        contentViewModel.renderStateError(R.string.main_repos_no_repos_found, false)
     }
 
-    fun onRetryClicked() {
-        retrySubject.onNext(Unit)
+    private fun renderStateErrorFetchData() {
+        contentViewModel.renderStateError(R.string.main_repos_error_fetching_data, true)
     }
 
     override fun onCleared() {
